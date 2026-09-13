13becf02c9b2f98fcb949a852bf4315e73d2c42f
rsujeetkumar011-dbr <rsujeetkumar011@gmail.com>
2026-09-13 19:38:24 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: '/aqua-Engineer/',
+})
+
+
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
